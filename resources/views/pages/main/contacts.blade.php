847f182fef973cf0928b44c5b1c4b03d086bf0be
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-10-16 12:19:43 +0300

    Phone removed from contact data

--- resources/views/pages/main/contacts.blade.php
+++ resources/views/pages/main/contacts.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <h2>{{ __('Contacts') }}</h2>
     <div id="contactdata">
-        <p>{{ __('Phone') }}: <span class="phone">+7 (222) 222-3-222</span></p>
+        {{-- <p>{{ __('Phone') }}: <span class="phone">+7 (222) 222-3-222</span></p> --}}
         <p>{{ __('Email') }}: <span class="phone">mike.wazovzky@gmail.com</span></p>
     </div><!-- contactdata -->
 </div><!-- container -->
