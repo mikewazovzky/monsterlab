424404e78e6ddda41c043ece64daf2f90bdf99cd
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-10-27 09:01:52 +0300

    Test post slugs are unique

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -10,10 +10,20 @@ class PostTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function it_creates_a_slug()
+    public function post_has_a_slug()
     {
         $post = create('App\Post');
 
         $this->assertEquals(str_slug($post->title), $post->fresh()->slug);
+    }
+
+    /** @test */
+    public function post_slug_is_unique()
+    {
+        $title = "Some title";
+        $postOne = create('App\Post', ['title' => $title]);
+        $postTwo = create('App\Post', ['title' => $title]);
+
+        $this->assertNotEquals($postOne->slug, $postTwo->slug);
     }
 }
