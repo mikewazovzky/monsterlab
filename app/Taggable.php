5cfea47293d1606591280b67cd34f154de00ccd2
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-11-16 16:45:59 +0300

    Tags: trait Taggable

---
+++ app/Taggable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use App\Tag;
+
+trait Taggable
+{
+    /**
+     * Update Model boot method.
+     * Detach all tags (and clean pivot table) when deleting the model.
+     *
+     * @param type name
+     * @return type
+     */
+    protected static function bootTaggable()
+    {
+        static::deleted(function ($model) {
+            $model->tags()->detach();
+        });
+    }
+
+    /**
+     * Get tags attached to the post.
+     *
+     * @return Illuminate\Database\Eloquent\Relations\belongsToMany
+     */
+    public function tags()
+    {
+        // return $this->belongsToMany(Tag::class);
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    /**
+     * Sync post tags.
+     * Only valid tags are added to post.
+     *
+     * @param array $tagList - array of tags (names)
+     * @return void
+     */
+    public function syncTags($tagList = [])
+    {
+        $this->tags()->sync(Tag::validate($tagList));
+    }
+}
